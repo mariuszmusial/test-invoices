19b6e7ae24bb7689078e1a4c972a5a1d6685d411
rafalwawrzyniak <rafal.wawrzyniak@onet.pl>
2018-07-12 23:30:32 +0200

    HelloWorldCommit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        greeting();
+    }
+
+    public static String greeting() {
+        return ("Hello World!");
+    }
+}
---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,17 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+    @Test
+    public void shouldCheckIfGreetingIsPrinted() {
+        //given
+        String expected = "Hello World!";
+
+        //hen
+        String actual = HelloWorld.greeting();
+
+        //then
+        assertEquals(expected, actual);
+    }
+}
