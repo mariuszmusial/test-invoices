1edd25addba60f779199e08ff578cd2cf633fad9
Wojciech Jarosz <wojtek_j@o2.pl>
2018-08-31 16:41:34 +0200

    Update InvoiceEntryValidatorTest.java

--- src/test/java/pl/coderstrust/invoices/validator/InvoiceEntryValidatorTest.java
+++ src/test/java/pl/coderstrust/invoices/validator/InvoiceEntryValidatorTest.java
@@ -1,6 +1,5 @@
 package pl.coderstrust.invoices.validator;
 
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
