e60d65c438f21f8f975d7c6e79366bbf8e374c10
wjarosz <wjarosz@novomatic-tech.com>
2018-09-13 17:54:54 +0200

    Created and fixed tests for InMemoryDatabase

--- src/test/java/pl/coderstrust/invoices/helper/TestData.java
+++ src/test/java/pl/coderstrust/invoices/helper/TestData.java
@@ -16,7 +16,11 @@ public class TestData {
   }
 
   public static Invoice sampleInvoice() {
-    return new Invoice(11L,
+    return sampleInvoice(11L);
+  }
+
+  public static Invoice sampleInvoice(Long id) {
+    return new Invoice(id,
         "test-invoice",
         LocalDate.of(2018, 5, 10),
         sampleCompany(),
@@ -35,19 +39,19 @@ public class TestData {
         + "\"vat\":\"VAT_23\"}]}";
   }
 
-  private static List<InvoiceEntry> sampleInvoiceEntries() {
+  public static List<InvoiceEntry> sampleInvoiceEntries() {
     return Arrays.asList(
         new InvoiceEntry("testProduct", 2, BigDecimal.valueOf(2.47d), Vat.VAT_5),
         new InvoiceEntry("testProduct 2", 10, BigDecimal.valueOf(1.92d), Vat.VAT_23)
     );
   }
 
-  private static Company sampleCompany2() {
+  public static Company sampleCompany2() {
     return new Company("company1", "959-294-43-78",
         "Marszałkowska 5", "25-440", "Kraków");
   }
 
-  private static Company sampleCompany() {
+  public static Company sampleCompany() {
     return new Company("company", "767-294-43-78",
         "Królewska 25", "30-440", "Warszawa");
   }
