b67c59bb0cbd1b9f3fda894cdcbfb6b978b2e146
Renata <renna@poczta.onet.pl>
2018-09-26 22:09:52 +0200

    InFileDatabase refactor after tests

--- src/main/java/pl/coderstrust/invoices/db/InFileDatabase.java
+++ src/main/java/pl/coderstrust/invoices/db/InFileDatabase.java
@@ -32,10 +32,6 @@ public class InFileDatabase implements Database {
     }
   }
 
-  // testy:
-  // 1. zapisujesz jednego przykladowego Invoice'a, sprawdzasz czy zostal zapisany
-  // 2. zapisujesz kilka invoiceow, sprawdzasz, czy plik z ID zawiera odpowiednie ID, czy sie zwieksza
-  // 3. Co jesli invoice jest nullem
   @Override
   public Long saveInvoice(Invoice invoice) throws DatabaseException {
     long newId;
@@ -43,14 +39,12 @@ public class InFileDatabase implements Database {
       String invoiceJson = InvoiceJsonMapper.toJson(invoice);
       newId = getAndIncrementId();
       FileHelper.writeLineToFile(databaseFile, invoiceJson);
-    } catch (Exception e) {
-      throw new DatabaseException(e);
+    } catch (Exception exception) {
+      throw new DatabaseException(exception);
     }
     return newId;
   }
 
-  //testy:
-  // 1. Zadne, bo to jest metoda prywatna
   private Long getAndIncrementId() throws FileNotFoundException {
     List<String> lines = FileHelper.readLinesFromFile(idFile);
     Long id = lines.isEmpty() ? -1L : Long.valueOf(lines.get(0));
@@ -59,10 +53,6 @@ public class InFileDatabase implements Database {
     return id;
   }
 
-  // testy:
-  // 1. Przygotowujesz plik z invoice'ami w formacie JSON, wolasz te metode i sprwadzasz czy zwrocila Ci wszystkie te invoice'y z pliku
-  // 2. Co zwroci metoda gdy w bazie nie bedzie zadnego invoice'a
-  // 3. Co zwroci metoda, gdy ktorys invoice (ktoras linijka w pliku) bedzie zle sformatowana
   @Override
   public List<Invoice> getInvoices() throws DatabaseException {
     List<String> lines;
@@ -73,22 +63,18 @@ public class InFileDatabase implements Database {
         Invoice invoice = InvoiceJsonMapper.fromJson(line);
         result.add(invoice);
       }
-    } catch (Exception e) {
-      throw new DatabaseException(e);
+    } catch (Exception exception) {
+      throw new DatabaseException(exception);
     }
     return result;
   }
 
-  // testy:
-  // 1. Co gdy ktos probuje zrobic update invoice'a, a nie podał ID
-  // 2. Sprawdz czy na pewno ten invoice sie zaktualizowal (najlepiej zmienic wszystkie pola w nim)
-  // 3. Powinien rzucic wyjatkiem gdy probuje sie update'owac invoice'a ktorego nei ma w bazie
   @Override
   public void updateInvoice(Invoice invoice) throws DatabaseException, NullPointerException {
-    Optional<Invoice> currentInvoice = null;
-    currentInvoice = getInvoiceById(invoice.getId());
+    Optional<Invoice> currentInvoice = getInvoiceById(invoice.getId());
     if (!currentInvoice.isPresent()) {
-      throw new DatabaseException("Invoice with given ID does not exist.");
+      throw new DatabaseException("Invoice with ID " + invoice.getId()
+          + " that you are trying to update does not exist in the database.");
     }
     List<Invoice> invoicesAfterUpdate = getInvoices().stream().filter(inv -> !inv.getId()
         .equals(invoice.getId())).collect(Collectors.toList());
@@ -110,20 +96,11 @@ public class InFileDatabase implements Database {
     return invoicesAsJson;
   }
 
-  // testy:
-  // 1. Przygotuj baze danych z kilkoma Invoice'ami, zeby mialy rozne daty
-  // i sprawdz czy znajdzie dobrze invoice'y miedzy danymi datami
-  // 2. Jesli nie poda sie startDate, to powinien zwrocic wszystkie az do endDate
-  // 3. Jesli nie poda sie endDate, to powinien zwrocic wszystkie od startDate (np. jak podalem 01-01-2018, null, to powinien znalezc mi wszystkie invoice'y z tego roku)
-  @Override
   public Collection<Invoice> findInvoicesByDateRange(LocalDate startDate, LocalDate endDate)
       throws DatabaseException {
     return InvoiceUtil.getInvoicesByDateRange(startDate, endDate, getInvoices());
   }
 
-  // testy
-  // 1. Nie powinien usunac niczego jesli sie poda ID ktorego nie ma w bazie
-  // 2. Powinien usunac rekord z bazy o zadanym ID
   @Override
   public void deleteInvoice(Long id) throws DatabaseException {
     List<Invoice> invoices = getInvoices().stream().filter(inv -> !inv.getId()
@@ -132,19 +109,17 @@ public class InFileDatabase implements Database {
     FileHelper.writeLinesToFile(databaseFile, invoicesAsJson);
   }
 
-  // testy
-  // 1. Powinien zwrocic invoice znaleziony z danym ID
-  // 2. Powinien zwrocic pustego optionala jesli dnaego ID nie ma w bazie
   @Override
   public Optional<Invoice> getInvoiceById(Long id) throws DatabaseException {
-    List<String> lines; // regex101.com
+    List<String> lines;
     try {
       lines = FileHelper.readLinesFromFile(databaseFile);
     } catch (FileNotFoundException fileNotFoundException) {
       throw new DatabaseException(fileNotFoundException);
     }
-    List<String> foundInvoices = lines.stream().filter(line -> line.startsWith("(\"id\":" + id))
-        .filter(line -> line.matches("$(\"id\":" + id + "\\s")).collect(Collectors.toList());
+    List<String> foundInvoices = lines.stream()
+        .filter(line -> line.startsWith("{\"id\":" + id + ","))
+        .collect(Collectors.toList());
     if (foundInvoices.size() > 1) {
       throw new DatabaseException("Found more than one invoice with given ID.");
     }
